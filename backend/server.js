6fb37b0077985f6c88755a84ef60a14ee92881b5
tesqule <super_g2006@email.com>
2026-03-04 20:44:58 +0300

    fix port

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
